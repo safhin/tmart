1da1088428516911c27e1332fb8f2f45ff694d64
Zahid Hasan <mdjahidhasan920@gmail.com>
2021-02-25 21:44:04 +0600

    Project ivew rearranged

--- app/Http/Controllers/Frontend/LandingPageController.php
+++ app/Http/Controllers/Frontend/LandingPageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
@@ -11,12 +12,10 @@ class LandingPageController extends Controller
     public function index()
     {   
         $productRow1 = Product::inRandomOrder()->take(6)->get();
-        $productRow2 = Product::inRandomOrder()->take(3)->get();
         $featuredSlider = Product::inRandomOrder()->take(4)->get();
         return view('frontend.landing',[
             'productRow1' => $productRow1,
-            'productRow2' => $productRow2,
-            'featuredSlider' => $featuredSlider
+            'featuredSlider' => $featuredSlider,
         ]);
     }
 }
